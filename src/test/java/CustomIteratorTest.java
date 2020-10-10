419516d8a9b542399ff390a859d1c60ef64c0c42
anatolikarman <anatolikarman@gmail.com>
2020-10-10 19:20:22 +0300

    Optimize tests a bit

--- src/test/java/CustomIteratorTest.java
+++ src/test/java/CustomIteratorTest.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.*;
@@ -7,30 +8,32 @@ public class CustomIteratorTest {
     private List<Integer> list;
     private List<Integer> resultList;
     private Iterator<Integer> iterator;
+    private EvenNumbersIterator customIterator;
+
+    @Before
+    public void initializeIteratorAndResultList() {
+        resultList = new ArrayList<Integer>();
+    }
 
     @Test
     public void allEvenNumbersList() {
         list = new ArrayList<Integer>(Arrays.asList(8, 4, 10, 12, 14));
-        resultList = new ArrayList<Integer>();
         iterator = list.iterator();
-        EvenNumbersIterator customIterator = new EvenNumbersIterator(iterator);
+        customIterator = new EvenNumbersIterator(iterator);
         while (customIterator.hasNext()) {
             resultList.add(customIterator.next());
         }
-
         Assert.assertArrayEquals(list.toArray(), resultList.toArray());
     }
 
     @Test
     public void allOddNumbers() {
         list = new ArrayList<Integer>(Arrays.asList(13, 5, 99, 11, 33));
-        resultList = new ArrayList<Integer>();
         iterator = list.iterator();
-        EvenNumbersIterator customIterator = new EvenNumbersIterator(iterator);
+        customIterator = new EvenNumbersIterator(iterator);
         while (customIterator.hasNext()) {
             resultList.add(customIterator.next());
         }
-
         Assert.assertTrue(resultList.isEmpty());
     }
 
