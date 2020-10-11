d8c204ace4e17bbad481e916257d5176d7542cc8
anatolikarman <anatolikarman@gmail.com>
2020-10-11 20:35:42 +0300

    Removed old annotations

--- src/main/java/EvenNumbersIterator.java
+++ src/main/java/EvenNumbersIterator.java
@@ -1,32 +1,18 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-/**
- * This iterator implementation takes in an iterator of integers.
- * <p>
- * This implementation will iterate over even integers only.
- */
+
 public class EvenNumbersIterator implements Iterator {
     private Iterator<Integer> iterator;
     private boolean hasNextCalled;
     private Integer next;
 
 
-    /**
-     * Constructor which takes in an Iterator of Integers.
-     *
-     * @param iterator - Iterator of integers.
-     * @throws RuntimeException - Throws runtime exception if the iterator being passed is null.
-     */
     public EvenNumbersIterator(Iterator<Integer> iterator) {
         this.iterator = iterator;
     }
 
-    /**
-     * This returns if the iterator has the next element.
-     *
-     * @return boolean
-     */
+
     @Override
     public boolean hasNext() {
         hasNextCalled = false;
